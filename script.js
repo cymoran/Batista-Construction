44f8347091a6bb095e02f152ed3dcb6c3ac493ee
cymoran <101744577+cymoran@users.noreply.github.com>
2024-05-18 12:41:31 -0400

    Img & Quality
    Added images and quality update for mobile devices

--- script.js
+++ script.js
@@ -14,15 +14,37 @@
     }
 }
 
-let w = window.innerWidth;
-let h = window.innerHeight;
-let serv_img = document.querySelectorAll(".serv-img")
+let slideIndex = 1;
+let timer = null;
 
-if (w <= 800){
-    document.querySelectorAll(".serv-img").style.width = '100%';
-    document.getElementById(".serv-img").style.height = '80px';
+showSlides(slideIndex);
 
+function plusSlides(n){
+    clearTimeout(timer);
+    showSlides(slideIndex += n);
 }
 
+function currentSlide(n){
+clearTimeout(timer);
+showSlides(slideIndex = n)
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n==undefined){n = ++slideIndex}
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    timer = setTimeout(showSlides, 5000);
+  } 
 
 
