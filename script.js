2d490100fc2c4b1055aec942a5ab2be4346f0bf8
cymoran <cymoran6@gmail.com>
2024-05-18 23:03:17 -0400

    hamb menu

--- script.js
+++ script.js
@@ -4,12 +4,12 @@
 
     if(document.body.scrollTop > 80 || document.documentElement.scrollTop >80){
         document.getElementById("navbar").style.padding = "0px";
-        document.getElementById("head").style.fontSize = "15px";
+        document.getElementById("head_nav").style.fontSize = "15px";
         document.getElementById("logo_img").style.width = "30px";
 
     } else{
         document.getElementById("navbar").style.padding = "10px";
-        document.getElementById("head").style.fontSize = "20px";
+        document.getElementById("head_nav").style.fontSize = "20px";
         document.getElementById("logo_img").style.width = "50px";
     }
 }
